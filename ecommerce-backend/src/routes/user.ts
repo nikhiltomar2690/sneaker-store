366acceadf113ae03839d3d7fc8121eb8250a01d
nikhiltomar2690 <nikhiltomar2690@gmail.com>
2024-06-21 13:42:36 +0530

    only admin access enabled

--- ecommerce-backend/src/routes/user.ts
+++ ecommerce-backend/src/routes/user.ts
@@ -5,6 +5,7 @@ import {
   getUser,
   newUser,
 } from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
 
 const app = express();
 
@@ -14,12 +15,12 @@ app.post("/new", newUser);
 
 // route - /api/v1/user/all
 // // usecase of this route : get all users
-app.get("/all", getAllUsers);
+app.get("/all", adminOnly, getAllUsers);
 
 // route - /api/v1/user/:id
 // usecase of this route : get user by id
 // app.get("/:id", getUser);
 // app.delete("/:id", deleteUser);
-app.route("/:id").get(getUser).delete(deleteUser);
+app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
 
 export default app;
